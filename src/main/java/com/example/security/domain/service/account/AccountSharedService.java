ade29ea623fcb470579d3b09111caca3445a7e4f
honzan <honzan0821@gmail.com>
2015-10-16 01:06:44 +0900

    チュートリアル完成

---
+++ src/main/java/com/example/security/domain/service/account/AccountSharedService.java
@@ -0,0 +1,10 @@
+package com.example.security.domain.service.account;
+
+import com.example.security.domain.model.Account;
+
+
+public interface AccountSharedService {
+
+    Account findOne(String username);
+
+}
